0644f95f06ffede493f4e1e5ef9c0e7584936b38
Subroto <subroto@bangthetable.com>
2018-03-22 07:42:57 +0530

    Updating Init Script

--- init_script.sh
+++ init_script.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# FIRST Use Ansible to Install all the Import Softwares and Command Line Tools
+
+# Use git clone to Clone the Repo first in default Folder, mine here is Subroto
+PATH_LOCAL_HOME="${HOME}/Subroto";
+if [ ! -d ${PATH_LOCAL_HOME} ]; then
+  mkdir ${PATH_LOCAL_HOME};
+cd ${PATH_LOCAL_HOME}; git clone git@github.com:Shub1427/dotfiles.git
+
 #####################################################################
 #
 # Create Symlinks if not present
@@ -14,10 +22,18 @@ PATH_ZSHRC="$HOME/.zshrc"
 PATH_ATOM="$HOME/.atom/config.cson"
 if [ ! -L $PATH_VSCODE ] && [ ! -L $PATH_ZSHRC ]; then
   echo "Symlinks Does not Exists, Creating them...";
-  ln -s ${HOME}/Subroto/dotfiles/.zshrc $PATH_ZSHRC;
-  ln -s ${HOME}/Subroto/dotfiles/vscode.json $PATH_VSCODE;
+  ln -s ${PATH_LOCAL_HOME}/dotfiles/.zshrc $PATH_ZSHRC;
+  ln -s ${PATH_LOCAL_HOME}/dotfiles/vscode.json $PATH_VSCODE;
   rm ${PATH_ATOM} #Remove if already present...
-  ln -s ${HOME}/Subroto/dotfiles/atom.cson $PATH_VSCODE;
+  ln -s ${PATH_LOCAL_HOME}/dotfiles/atom.cson $PATH_VSCODE;
 else
   echo "Symlinks Exists..."
 fi
+
+#####################################################################
+#
+# Download and Install Atom Packages
+#
+#####################################################################
+$(apm install copy-path file-icons highlight-selected local-history related set-syntax atom-wrap-in-tag atom-ternjs autoclose-html autocomplete-modules docblockr emmet emmet-jsx-css-modules es6-javascript js-hyperclick hyperclick tree-view-copy-relative-path
+language-babel react-es7-snippets atom-jest-snippets);
