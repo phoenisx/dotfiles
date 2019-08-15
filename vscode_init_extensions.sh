193d9358b262a4e1509de4f33fb8b2c31991b5f4
Shub1427 <shub1493biswas@gmail.com>
2019-08-16 01:04:29 +0530

    Update Polybar configs with more modules
    * Remove unnecessary vscode extensions

--- vscode_init_extensions.sh
+++ vscode_init_extensions.sh
@@ -3,29 +3,13 @@
 # Run the Following cmd to regenerate this file...
 # for ext in $(code --list-extensions); do; echo "code --install-extension ${ext}"; done > vscode_init_extensions.sh
 
-code --install-extension adamwalzer.scss-lint
 code --install-extension blanu.vscode-styled-jsx
-code --install-extension castwide.solargraph
-code --install-extension chenxsan.vscode-standardjs
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension CraigMaslowski.erb
 code --install-extension cybai.yaml-key-viewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
-code --install-extension eg2.vscode-npm-script
 code --install-extension euskadi31.json-pretty-printer
-code --install-extension formulahendry.code-runner
-code --install-extension jasonnutter.search-node-modules
-code --install-extension joelday.docthis
-code --install-extension k--kato.intellij-idea-keybindings
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.sublime-keybindings
-code --install-extension rebornix.ruby
 code --install-extension steoates.autoimport
-code --install-extension vayan.haml
 code --install-extension waderyan.gitblame
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension xabikos.JavaScriptSnippets
 code --install-extension xabikos.ReactSnippets
-
