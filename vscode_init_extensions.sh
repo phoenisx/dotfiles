84b155551d7b8e39b200de8ba81ea4beba500244
Shub <shub1493biswas@gmail.com>
2021-05-18 12:01:41 +0530

    feat: support new font and required extensions on VSCode

--- vscode_init_extensions.sh
+++ vscode_init_extensions.sh
@@ -3,13 +3,14 @@
 # Run the Following cmd to regenerate this file...
 # for ext in $(code --list-extensions); do; echo "code --install-extension ${ext}"; done > vscode_init_extensions.sh
 
-code --install-extension blanu.vscode-styled-jsx
-code --install-extension cybai.yaml-key-viewer
+# code --install-extension blanu.vscode-styled-jsx
+# code --install-extension cybai.yaml-key-viewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension euskadi31.json-pretty-printer
-code --install-extension ms-vscode.cpptools
+# code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.sublime-keybindings
-code --install-extension steoates.autoimport
-code --install-extension waderyan.gitblame
-code --install-extension xabikos.ReactSnippets
+# code --install-extension steoates.autoimport
+# code --install-extension waderyan.gitblame
+# code --install-extension xabikos.ReactSnippets
+code --install-extension phoenisx.cssvar
