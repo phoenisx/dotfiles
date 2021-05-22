a41614dc648e339bafe00a83e99d1d6c60a916e8
Shub <shub1493biswas@gmail.com>
2021-05-22 11:43:27 +0530

    feat: support deno binary path|update global vscode settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,18 @@
 . ~/.config/fish/docker.alias.fish
 . ~/.config/fish/misc.alias.fish
 
+switch (uname)
+case Linux
+  # Need to change this
+  set -g DENO_INSTALL "/Users/shub/.deno"
+case Darwin
+  set -g DENO_INSTALL "/Users/shub/.deno"
+case '*'
+  set -g DENO_INSTALL "/Users/shub/.deno"
+end
+
 # Theme Settings for OMF
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
-set -gx PATH ~/go/bin/ ~/.cargo/bin /usr/local/bin $PATH
+
+set -gx PATH ~/go/bin/ ~/.cargo/bin (echo "$DENO_INSTALL/bin") /usr/local/bin $PATH
