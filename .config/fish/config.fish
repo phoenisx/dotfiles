28908dc0f406e4f1e0d73790b296affe85f5a486
Shub <shub1493biswas@gmail.com>
2021-06-16 17:15:51 +0530

    fix: alacritty/fish integration, installation fix for budspencer theme on MacOS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,9 @@ case Linux
   set -g DENO_INSTALL "/Users/shub/.deno"
 case Darwin
   set -g DENO_INSTALL "/Users/shub/.deno"
+  # Check this for details: https://github.com/oh-my-fish/theme-budspencer/issues/57#issuecomment-551452088
+  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 case '*'
   set -g DENO_INSTALL "/Users/shub/.deno"
 end
