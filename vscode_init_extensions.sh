c4e8a4776e462afb402e1daae32cca9326c6e185
Shub <shub1493biswas@gmail.com>
2021-06-16 17:15:39 +0530

    feat: update vscode extension list and vscode settings

--- vscode_init_extensions.sh
+++ vscode_init_extensions.sh
@@ -3,14 +3,12 @@
 # Run the Following cmd to regenerate this file...
 # for ext in $(code --list-extensions); do; echo "code --install-extension ${ext}"; done > vscode_init_extensions.sh
 
-# code --install-extension blanu.vscode-styled-jsx
-# code --install-extension cybai.yaml-key-viewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension euskadi31.json-pretty-printer
-# code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.sublime-keybindings
-# code --install-extension steoates.autoimport
-# code --install-extension waderyan.gitblame
-# code --install-extension xabikos.ReactSnippets
 code --install-extension phoenisx.cssvar
+code --install-extension denoland.vscode-deno
+code --install-extension esbenp.prettier-vscode
+code --install-extension matklad.rust-analyzer
+code --install-extension rust-lang.rust
