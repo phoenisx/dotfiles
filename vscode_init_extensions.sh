aba32f0da498b98bf0355c5c04619276af101fed
Shub <shub1493biswas@gmail.com>
2021-11-18 12:26:56 +0530

    feat: add new vscode extensions

--- vscode_init_extensions.sh
+++ vscode_init_extensions.sh
@@ -4,11 +4,19 @@
 # for ext in $(code --list-extensions); do; echo "code --install-extension ${ext}"; done > vscode_init_extensions.sh
 
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension denoland.vscode-deno
 code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
 code --install-extension euskadi31.json-pretty-printer
+code --install-extension matklad.rust-analyzer
+code --install-extension mikestead.dotenv
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension phoenisx.cssvar
-code --install-extension denoland.vscode-deno
-code --install-extension esbenp.prettier-vscode
-code --install-extension matklad.rust-analyzer
+code --install-extension phoenisx.gohugo
+code --install-extension redhat.vscode-yaml
 code --install-extension rust-lang.rust
+code --install-extension skyapps.fish-vscode
+code --install-extension svelte.svelte-vscode
+code --install-extension tamasfe.even-better-toml
+code --install-extension Tyriar.lorem-ipsum
+code --install-extension yzhang.markdown-all-in-one
