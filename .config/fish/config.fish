af1b55d41e2e9bf1b1db051a293afcab601b2c2a
Shub <shub1493biswas@gmail.com>
2021-12-15 11:19:48 +0530

    feat: support personal macbook

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,4 +19,5 @@ end
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 
-set -gx PATH ~/go/bin/ ~/.cargo/bin (echo "$DENO_INSTALL/bin") /usr/local/bin $PATH
+set -gx NVM_DIR (brew --prefix nvm)
+set -gx PATH ~/go/bin/ ~/.cargo/bin (echo "$DENO_INSTALL/bin") /opt/homebrew/bin/ /usr/local/bin $PATH
