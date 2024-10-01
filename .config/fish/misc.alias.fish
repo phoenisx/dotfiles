d534f03c200b98e976247d243d23ebaaa2b75ece
Subroto <subroto.biswas@postman.com>
2024-10-01 12:38:11 +0530

    feat: fish|vscode|tmux update

--- .config/fish/misc.alias.fish
+++ .config/fish/misc.alias.fish
@@ -12,3 +12,4 @@ alias pn="pnpm"
 # Ref: https://unix.stackexchange.com/a/48201
 # Don't want to remove a file by mistake :facepalm:
 alias rm="rm -i"
+alias curltime="curl -sw \"@$HOME/Downloads/Downloads/Postman_Apps_for_Testing/Performance Profile Curl/curl-format.txt\" -o /dev/null"
